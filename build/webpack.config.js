1f92b22d3d1402a897d56d849fee18c4da09cff3
niuzz <niuzz@hotmail.com>
2018-03-06 13:46:00 +0800

    fixed babel react bug

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -14,8 +14,35 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /.css$/,
+				test: /.(css)$/,
 				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(scss)$/,
+				use: [{
+					loader: 'style-loader', // inject CSS to page
+				}, {
+					loader: 'css-loader', // translates CSS into CommonJS modules
+				}, {
+					loader: 'postcss-loader', // Run post css actions
+					options: {
+						plugins: function () { // post css plugins, can be exported to postcss.config.js
+							return [
+								require('precss'),
+								require('autoprefixer')
+							];
+						}
+					}
+				}, {
+					loader: 'sass-loader' // compiles Sass to CSS
+				}]
+			},
+			{
+				test: /\.(js)$/,
+				loader: 'babel-loader',
+				exclude: [
+					path.join(__dirname, '../node_modules')
+				]
 			}
 		]
 	},
