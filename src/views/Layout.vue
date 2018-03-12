b533a1df0ff2ef619ba96bff4d0ec1be9a612087
niuzz <niuzz@hotmail.com>
2018-03-12 14:48:51 +0800

    add vue

---
+++ src/views/Layout.vue
@@ -0,0 +1,19 @@
+<!-- ******************************************
+*  Author : niuzz niuzz@hotmail.com
+*  Created On : Mon Mar 12 2018
+******************************************* -->
+<template>
+	<div class="wrap">
+		{{ message }}
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+	data: () => ({message: 'Layout VUE is here!!!!'})
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
