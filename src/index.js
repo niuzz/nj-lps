09a35615c698dfc41be22e89df8eb40f81c5007b
niuzz <niuzz@hotmail.com>
2018-06-05 14:50:01 +0800

    menu list

--- src/index.js
+++ src/index.js
@@ -3,10 +3,43 @@ let sp = {
 	name: 'ss'
 }
 
-const fun = () => {
-	console.log('中文')
-}
+var isFold = true
 
 window.onload = function () {
-	fun()
+	calcWidth()
 }
+
+function calcWidth () {
+	let clientWidth = document.body.clientWidth
+	let navIcon = document.querySelector('.nav-list')
+	let container = document.querySelector('.container')
+
+	navIcon.addEventListener('click', function (e) {
+		let ul = e.target.parentElement.querySelector('ul')
+		if (isFold) {
+			e.target.classList.remove('nj-nav-list-o')
+			e.target.classList.add('nj-close-o')
+			ul.classList.remove('hide')
+			ul.classList.add('show')
+			isFold = false
+		} else {
+			e.target.classList.remove('nj-close-o')
+			e.target.classList.add('nj-nav-list-o')
+			ul.classList.remove('show')
+			ul.classList.add('hide')
+			isFold = true
+		}
+	}, false)
+	
+	if (clientWidth > 450) {
+		container.classList.remove('hide')
+		navIcon.classList.add('hide')
+		isFold = false
+	} else {
+		navIcon.classList.remove('hide')
+		container.classList.add('hide')
+		isFold = true
+	}
+}
+
+window.addEventListener('resize', calcWidth, false)
